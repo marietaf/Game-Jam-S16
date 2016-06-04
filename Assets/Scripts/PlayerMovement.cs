7b5f0978ce0963c7a0b1b7b885dfd35a97303603
Ian Holstead <ian.holstead@gmail.com>
2016-06-03 22:01:18 -0400

    Afte being sassed by the people beside me, fixed a mino typo

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using Prime31;
 
 public class PlayerMovement : MonoBehaviour {
-    
+
     // movement config
+    //public bool isGravityDown = true;
     public float gravity = -25f;
     public float runSpeed = 8f;
     public float groundDamping = 20f; // how fast do we change direction? higher means faster
@@ -34,12 +35,12 @@ public class PlayerMovement : MonoBehaviour {
 	
 	}
 	
-	public void Move(float hoizontalMovement, bool jump, bool dropDown) {
+	public void Move(float horizontalMovement, bool jump, bool dropDown) {
         if (characterController.isGrounded)
         {
             velocity.y = 0;
         }
-        normalizedHorizontalSpeed = hoizontalMovement;
+        normalizedHorizontalSpeed = horizontalMovement;
         if (transform.localScale.x > 0f)
         {
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
@@ -48,7 +49,7 @@ public class PlayerMovement : MonoBehaviour {
         {
             //_animator.Play( Animator.StringToHash( "Run" ) );
         }
-        if (hoizontalMovement == 0)
+        if (horizontalMovement == 0)
         {
             if (characterController.isGrounded)
             {
@@ -59,14 +60,15 @@ public class PlayerMovement : MonoBehaviour {
         // we can only jump whilst grounded
         if (characterController.isGrounded && jump)
         {
-            velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
+            Debug.Log(Mathf.Sign(-gravity) * Mathf.Sqrt(2f * jumpHeight * Mathf.Abs(gravity)));
+            velocity.y = Mathf.Sign(-gravity) * Mathf.Sqrt(2f * jumpHeight * Mathf.Abs(gravity));
             //_animator.Play( Animator.StringToHash( "Jump" ) );
         }
 
 
         // apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
         var smoothedMovementFactor = characterController.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
-        velocity.x = Mathf.Lerp(velocity.x, hoizontalMovement * runSpeed, Time.deltaTime * smoothedMovementFactor);
+        velocity.x = Mathf.Lerp(velocity.x, horizontalMovement * runSpeed, Time.deltaTime * smoothedMovementFactor);
 
         // apply gravity before moving
         velocity.y += gravity * Time.deltaTime;
