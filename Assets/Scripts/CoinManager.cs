239e601dc2e355098944fa548911b9ca064c361c
Marieta Farova <m2farova@uwaterloo.ca>
2016-06-05 09:36:14 -0400

    Added coins to the game
    Steps:
    - Add GameController to the scene
    - Add text to the screen
    - Set text property in the GameController
    - Add coins

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinManager : MonoBehaviour {
+
+    private GameController gc;
+	
+    void Start ()
+    {
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+	void OnTriggerEnter2D (Collider2D col)
+    {
+	    if(col.CompareTag("Player") || col.CompareTag("PlayerClone") && gc != null)
+        {
+            Destroy(gameObject);
+            gc.points++;
+        }
+	}
+}
