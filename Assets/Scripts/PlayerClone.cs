ac7a3f6385a8f05992a83fc73f2963af610380c5
Marieta Farova <m2farova@uwaterloo.ca>
2016-06-05 13:04:42 -0400

    Fixed camera following when SP with/without clone and MP

--- Assets/Scripts/PlayerClone.cs
+++ Assets/Scripts/PlayerClone.cs
@@ -6,30 +6,31 @@ public class PlayerClone : MonoBehaviour {
 
     public GameObject clonePrefab;
 
-    private bool isCloneInstantiated;
-    GameObject clone;
+    [HideInInspector]
+    public bool isCloneActive;
+    [HideInInspector]
+    public GameObject clone;
 
     public void Start()
     {
-        isCloneInstantiated = false;
+        isCloneActive = false;
     }
 
 	public void ToggleClone (bool togglePressed) {
         if(togglePressed)
         {
-            if (!isCloneInstantiated) {
-                isCloneInstantiated = true;
+            if (!isCloneActive) {
+                isCloneActive = true;
                 if (clone == null)
                 {
                     clone = (GameObject)Instantiate(clonePrefab, transform.position + new Vector3(0, -1.02f, 0), transform.rotation);
                     PlayerMovement move = GetComponent<PlayerMovement>();
-                    clone.GetComponent<CloneMovement>().SetCloneValues(GetComponent<Transform>(), move.gravity, move.runSpeed, move.groundDamping, move.inAirDamping, move.jumpHeight);
+                    clone.GetComponent<CloneMovement>().SetCloneValues(GetComponent<Transform>(), -move.gravity, move.runSpeed, move.groundDamping, move.inAirDamping, move.jumpHeight);
                 }
                 clone.SetActive(true);
-                //clone.GetComponent<CloneMovement>().gravity = GetComponent<PlayerMovement>().gravity;
             } else
             {
-                isCloneInstantiated = false;
+                isCloneActive = false;
                 clone.SetActive(false);
             }
         }
--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -6,7 +6,7 @@ using Prime31;
 public class SmoothFollow : MonoBehaviour
 {
     public bool isTwoPlayer = false;
-    public Transform player1, player2;
+    public GameObject player1, player2;
     public float smoothDampTime = 0.2f;
     [HideInInspector]
     public new Transform transform;
@@ -16,6 +16,7 @@ public class SmoothFollow : MonoBehaviour
 
     private Camera _camera;
     private float _smoothSizeDampVelocity;
+    private float _sizeSmoothDampVelocity;
     private Vector3 _smoothDampVelocity;
 
 
@@ -44,16 +45,39 @@ public class SmoothFollow : MonoBehaviour
     {
         if(!isTwoPlayer)
         {
-            _camera.orthographicSize = minCameraSize;
-            transform.position = Vector3.SmoothDamp(transform.position, player1.position - cameraOffset, ref _smoothDampVelocity, smoothDampTime);
+            if(player1.GetComponent<PlayerClone>() != null && player1.GetComponent<PlayerClone>().isCloneActive)
+            {
+                // Single player & clone not active
+                Vector3 player1Position = player1.transform.position;
+                Vector3 clonePosition = player1.GetComponent<PlayerClone>().clone.transform.position;
+
+                float disBetweenPlayers = (player1Position - clonePosition).magnitude;
+                float newCameraSize = Mathf.Max(minCameraSize, Mathf.Min(maxCameraSize, disBetweenPlayers));
+                _camera.orthographicSize = Mathf.SmoothDamp(_camera.orthographicSize, newCameraSize, ref _smoothSizeDampVelocity, smoothDampTime);
+
+                Vector3 midpointPosition = (player1Position - clonePosition) * 0.5f + clonePosition;
+                transform.position = Vector3.SmoothDamp(transform.position, midpointPosition - cameraOffset, ref _smoothDampVelocity, smoothDampTime);
+            }
+            else
+            {
+                // Single player & clone not active
+                _camera.orthographicSize = Mathf.SmoothDamp(_camera.orthographicSize, minCameraSize, ref _sizeSmoothDampVelocity, smoothDampTime);
+                transform.position = Vector3.SmoothDamp(transform.position, player1.transform.position - cameraOffset, ref _smoothDampVelocity, smoothDampTime);
+            }
+
+            
         }
         else
         {
-            float disBetweenPlayers = (player1.position - player2.position).magnitude;
+            // Multiplayer
+            Vector3 player1Position = player1.transform.position;
+            Vector3 player2Position = player2.transform.position;
+
+            float disBetweenPlayers = (player1Position - player2Position).magnitude;
             float newCameraSize = Mathf.Max(minCameraSize, Mathf.Min(maxCameraSize, disBetweenPlayers ));
             _camera.orthographicSize = Mathf.SmoothDamp(_camera.orthographicSize, newCameraSize, ref _smoothSizeDampVelocity, smoothDampTime);
 
-            Vector3 midpointPosition = (player1.position - player2.position) * 0.5f + player2.position;
+            Vector3 midpointPosition = (player1Position - player2Position) * 0.5f + player2Position;
             transform.position = Vector3.SmoothDamp(transform.position, midpointPosition - cameraOffset, ref _smoothDampVelocity, smoothDampTime);
         }
     }
