5308557e0222710362f38fe26659279c4adee48a
Ian Holstead <ian.holstead@gmail.com>
2016-06-14 19:44:07 -0400

    Project organization and minor refactoring

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class CoinManager : MonoBehaviour {
 
-    private GameController gc;
+    private GameController gameController;
 	
     void Start ()
     {
-        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        //we could also put a ref to the gameController on the players, or track this stat in the players so we know 
+        //who collected the conins and we wont be makeing hundreds of calls to findgameobject
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
     }
 
 	void OnTriggerEnter2D (Collider2D col)
     {
-	    if(col.CompareTag("Player") || col.CompareTag("PlayerClone") && gc != null)
+	    if(col.CompareTag("Player") || col.CompareTag("PlayerClone"))
         {
             Destroy(gameObject);
-            gc.points++;
+            gameController.points++;
         }
 	}
 }
---
+++ Assets/Scripts/Input/ControllerInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ControllerInput : InputBaseClass {
+    public int playerNumber = 1;
+
+    public override bool GetJump()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool GetDropDown()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override float GetHorizontalMovement()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool GetToggleClone()
+    {
+        throw new NotImplementedException();
+    }
+}
